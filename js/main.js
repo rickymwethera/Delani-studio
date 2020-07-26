03cb421b8bb011d2f4b2813719ac864367e9e42a
rickymwethera <rickymwethera@gmail.com>
2020-07-26 13:34:25 +0300

    added hover effect on portfolio

--- js/main.js
+++ js/main.js
@@ -9,3 +9,45 @@ function submit() {
       alert("Thank you for reaching out to us "+names+", we have received your message.");
   }
 };
+$(document).ready(function() {
+  $(".h1").hover(function() {
+    $("#h1text").show("");
+  }, function() {
+    $("#h1text").hide("");
+  });
+  $(".h2").hover(function(){
+    $("#h2text").show("");
+  }, function() {
+    $("#h2text").hide("");
+  });
+  $(".h3").hover(function(){
+    $("#h3text").show("");
+  }, function() {
+    $("#h3text").hide("");
+  });
+  $(".h4").hover(function(){
+    $("#h4text").show("");
+  }, function() {
+    $("#h4text").hide("");
+  });
+  $(".h5").hover(function(){
+    $("#h5text").show("");
+  }, function() {
+    $("#h5text").hide("");
+  });
+  $(".h6").hover(function(){
+    $("#h6text").show("");
+  }, function() {
+    $("#h6text").hide("");
+  });
+  $(".h7").hover(function(){
+    $("#h7text").show("");
+  }, function() {
+    $("#h7text").hide("");
+  });
+  $(".h8").hover(function(){
+    $("#h8text").show("");
+  }, function() {
+    $("#h8text").hide("");
+  });
+});
