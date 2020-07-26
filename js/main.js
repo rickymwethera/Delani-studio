2c2911d22c35240bed7370f06a9d1c10856be998
rickymwethera <rickymwethera@gmail.com>
2020-07-26 13:04:34 +0300

    added popup for the form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
-    $('#productPhoto').click(function() {
-        $('#productText').toggle();
-        $('#productPhoto').toggle();
-    });
-});
+function submit() {
+  var names = document.getElementById("name").value;
+  var emails=document.getElementById("email").value;
+  var messages=document.getElementById("message").value;
+  if((names=="")||(emails=="")||(messages=="")){
+      alert("Please input all fields!");
+  }
+  else {
+      alert("Thank you for reaching out to us "+names+", we have received your message.");
+  }
+};
